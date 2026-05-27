90d51815128ef44c0feb0c24a2d6b53242c02adf
Ssinha254 <126397502+Ssinha254@users.noreply.github.com>
2026-05-27 13:38:19 +0530

    File updated at 5/27/2026, 1:38:18 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,48 +1,43 @@
-# Last updated: 4/21/2026, 11:26:55 PM
-class Solution(object):
-    def solve(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
-        def dfs( node, vis):
-            if node is None:
-                return
-            if node[0] + 1 < rows and vis[node[0] + 1][node[1]] == 0 and board[node[0] + 1][node[1]] == "O":
-                vis[node[0] + 1][node[1]] = 1
-                dfs([node[0] + 1,node[1]],vis)
-            if node[1] + 1< cols and vis[node[0]][node[1] + 1] == 0 and board[node[0]][node[1] + 1] == "O" :
-                vis[node[0]][node[1] + 1] = 1
-                dfs([node[0],node[1] + 1], vis)
-            if node[0] - 1 >= 0 and vis[node[0] - 1][node[1]] == 0 and board[node[0] - 1][node[1]] == "O":
-                vis[node[0] - 1][node[1]] = 1
-                dfs([node[0] - 1,node[1]], vis)
-            if node[1] - 1  >=0 and vis[node[0]][node[1] - 1] == 0 and board[node[0]][node[1] - 1] == "O":
-                vis[node[0]][node[1] - 1] = 1
-                dfs([node[0],node[1] - 1], vis)
-              
-        rows = len(board)
-        cols = len(board[0])
-        vis = [[0 for _ in range(cols)] for _ in range(rows)]
-
-        for i in range(rows):
-            if board[i][0] == "O" and vis[i][0] == 0:
-                vis[i][0] = 1
-                dfs([i, 0], vis)
-            if board[i][cols - 1] == "O" and vis[i][cols - 1] == 0:
-                vis[i][cols - 1] = 1
-                dfs([i, cols - 1], vis)
-
-        for i in range(cols):
-            if board[0][i] == "O" and vis[0][i] == 0:
-                vis[0][i] = 1
-                dfs([0, i], vis)
-            if board[rows - 1][i] == "O" and vis[rows - 1][i] == 0:
-                vis[rows - 1][i] = 1
-                dfs([rows - 1, i], vis)
-  
-
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == "O" and vis[i][j] == 0:
-                    board[i][j] ="X"
+# Last updated: 5/27/2026, 1:38:18 PM
+1class Solution(object):
+2    def solve(self, board):
+3        """
+4        :type board: List[List[str]]
+5        :rtype: None Do not return anything, modify board in-place instead.
+6        """
+7      
+8        n = len(board)
+9        m = len(board[0])
+10
+11        visited = set()
+12        def dfs(i,j):
+13            if (i,j) in visited:
+14                return
+15            visited.add((i,j))
+16            dirs=[(0,1), (1,0),(-1,0), (0,-1)]
+17            for r, c in dirs:
+18                ni = i + r
+19                nj = j + c
+20                if ni < 0 or ni >= n or nj < 0 or nj >= m:
+21                    continue
+22                if (ni,nj) in visited:
+23                    continue
+24                if board[ni][nj] == 'O':
+25                    dfs(ni,nj)
+26        
+27        for i in range(n):
+28            if board[i][0] == 'O':
+29                dfs(i, 0)
+30            if board[i][m - 1] =='O':
+31                dfs(i, m -1)
+32        for j in range(m):
+33            if board[0][j] == 'O':
+34                dfs(0,j)
+35            if board[n - 1][j] == 'O':
+36                dfs(n-1, j)
+37
+38        for i in range(n):
+39            for j in range(m):
+40                if (i,j) not in visited:
+41                    board[i][j] = 'X'
+42        return board
